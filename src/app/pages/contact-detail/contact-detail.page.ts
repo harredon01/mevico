ef5ccae2908346f3a2ae7d9940796d06bf7f5157
Hoovert Arredondo <harredon01@gmail.com>
2019-08-14 16:00:35 -0500

    Added contact detail and group detail page

--- src/app/pages/contact-detail/contact-detail.page.ts
+++ src/app/pages/contact-detail/contact-detail.page.ts
@@ -1,15 +1,73 @@
-import { Component, OnInit } from '@angular/core';
-
+import {Component, OnInit} from '@angular/core';
+import {ContactsService} from '../../services/contacts/contacts.service';
+import {NavController, LoadingController, ModalController} from '@ionic/angular';
+import {SpinnerDialog} from '@ionic-native/spinner-dialog/ngx';
+import {Contact} from '../../models/contact';
+import {ApiService} from '../../services/api/api.service';
+import {ActivatedRoute} from '@angular/router';
+import {ParamsService} from '../../services/params/params.service';
 @Component({
   selector: 'app-contact-detail',
   templateUrl: './contact-detail.page.html',
   styleUrls: ['./contact-detail.page.scss'],
 })
 export class ContactDetailPage implements OnInit {
+private mainContact: Contact;
+    constructor(public contacts: ContactsService,
+        public activatedRoute: ActivatedRoute,
+        public params: ParamsService,
+        public navCtrl: NavController,
+        public modalCtrl: ModalController,
+        public api: ApiService,
+        public loadingCtrl: LoadingController,
+        public spinnerDialog: SpinnerDialog
+    ) {}
 
-  constructor() { }
-
-  ngOnInit() {
-  }
-
+    ngOnInit() {
+        let params = this.params.getParams();
+        this.mainContact = params.item;
+    }
+    updateblockStatus(status:any) {
+        let data = {
+            "status":status,
+            "contact_id": this.mainContact.id
+        };
+        this.showLoader();
+        this.contacts.updateBlockStatus(data).subscribe((data: any) => {
+            this.mainContact.status = status;
+            this.dismissLoader();
+        }, (err) => {
+            console.log("Error cancelBooking");
+            this.dismissLoader();
+            this.api.handleError(err);
+        });
+    }
+    deleteContact() {
+        this.showLoader();
+        this.contacts.deleteContact(this.mainContact.id).subscribe((data: any) => {
+            this.navCtrl.back();
+            this.dismissLoader();
+        }, (err) => {
+            console.log("Error cancelBooking");
+            this.dismissLoader();
+            this.api.handleError(err);
+        });
+    }
+    dismissLoader() {
+        if (document.URL.startsWith('http')) {
+            this.loadingCtrl.dismiss();
+        } else {
+            this.spinnerDialog.hide();
+        }
+    }
+    showLoader() {
+        if (document.URL.startsWith('http')) {
+            this.loadingCtrl.create({
+                spinner: 'crescent',
+                backdropDismiss: true
+            }).then(toast => toast.present());
+        } else {
+            this.spinnerDialog.show();
+        }
+    }
 }
--- src/app/pages/group-detail/group-detail.page.ts
+++ src/app/pages/group-detail/group-detail.page.ts
@@ -1,4 +1,11 @@
-import { Component, OnInit } from '@angular/core';
+import {Component, OnInit} from '@angular/core';
+import {GroupsService} from '../../services/groups/groups.service';
+import {NavController, LoadingController, ModalController} from '@ionic/angular';
+import {SpinnerDialog} from '@ionic-native/spinner-dialog/ngx';
+import {Group} from '../../models/group';
+import {ApiService} from '../../services/api/api.service';
+import {ActivatedRoute} from '@angular/router';
+import {ParamsService} from '../../services/params/params.service';
 
 @Component({
   selector: 'app-group-detail',
@@ -6,10 +13,48 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./group-detail.page.scss'],
 })
 export class GroupDetailPage implements OnInit {
+private mainGroup: Group;
+    constructor(public groups: GroupsService,
+        public activatedRoute: ActivatedRoute,
+        public params: ParamsService,
+        public navCtrl: NavController,
+        public modalCtrl: ModalController,
+        public api: ApiService,
+        public loadingCtrl: LoadingController,
+        public spinnerDialog: SpinnerDialog
+    ) {}
 
-  constructor() { }
+    ngOnInit() {
+        let params = this.params.getParams();
+        this.mainGroup = params.item;
+    }
 
-  ngOnInit() {
-  }
-
+    leaveGroup() {
+        this.showLoader();
+        this.groups.leaveGroup(this.mainGroup.id).subscribe((data: any) => {
+            this.navCtrl.back();
+            this.dismissLoader();
+        }, (err) => {
+            console.log("Error leaveGroup");
+            this.dismissLoader();
+            this.api.handleError(err);
+        });
+    }
+    dismissLoader() {
+        if (document.URL.startsWith('http')) {
+            this.loadingCtrl.dismiss();
+        } else {
+            this.spinnerDialog.hide();
+        }
+    }
+    showLoader() {
+        if (document.URL.startsWith('http')) {
+            this.loadingCtrl.create({
+                spinner: 'crescent',
+                backdropDismiss: true
+            }).then(toast => toast.present());
+        } else {
+            this.spinnerDialog.show();
+        }
+    }
 }
