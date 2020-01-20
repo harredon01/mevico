f188eb9fe2200f3e0b62d6f35838800fe89e4d33
Hoovert <harredon01@gmail.com>
2020-01-19 21:16:36 -0500

    details for connection

--- src/app/pages/opentok/opentok.page.ts
+++ src/app/pages/opentok/opentok.page.ts
@@ -1,9 +1,10 @@
 import {Component, OnInit} from '@angular/core';
-import {NavController, ModalController} from '@ionic/angular';
+import {NavController, ModalController,LoadingController} from '@ionic/angular';
 import {ParamsService} from '../../services/params/params.service';
 import {BookingService} from '../../services/booking/booking.service';
 import {BookingDetailPage} from '../booking-detail/booking-detail.page';
-import {Booking} from '../../models/booking'
+import {Booking} from '../../models/booking';
+import {SpinnerDialog} from '@ionic-native/spinner-dialog/ngx';
 //import * as OT from '@opentok/client';
 declare var OT: any;
 @Component({
@@ -16,13 +17,15 @@ export class OpentokPage implements OnInit {
     publisher: any;
     apiKey: any;
     bookingId: any;
-    mainBooking:Booking;
+    mainBooking: Booking;
     sessionId: string;
     token: string;
     activeCall: boolean = false;
 
     constructor(public navCtrl: NavController,
         public modalCtrl: ModalController,
+        public loadingCtrl: LoadingController,
+        public spinnerDialog: SpinnerDialog,
         public params: ParamsService,
         public bookingS: BookingService) {
         this.apiKey = '46389642';
@@ -56,8 +59,12 @@ export class OpentokPage implements OnInit {
         let container = {};
         let el = (document.querySelector('.OT_publisher') as HTMLElement);
         let el2 = (document.querySelector('.OT_subscriber') as HTMLElement);
-        el.style.setProperty('display', 'none');
-        el2.style.setProperty('display', 'none');
+        if (el) {
+            el.style.setProperty('display', 'none');
+        }
+        if (el2) {
+            el2.style.setProperty('display', 'none');
+        }
         OT.updateViews();
         let params = this.params.getParams();
         if (!params) {
@@ -72,8 +79,12 @@ export class OpentokPage implements OnInit {
         await addModal.present();
         const {data} = await addModal.onDidDismiss();
         console.log("Modal Closed");
-        el.style.setProperty('display', 'block');
-        el2.style.setProperty('display', 'block');
+        if (el) {
+            el.style.setProperty('display', 'block');
+        }
+        if (el2) {
+            el2.style.setProperty('display', 'block');
+        }
         OT.updateViews();
     }
 
@@ -83,16 +94,18 @@ export class OpentokPage implements OnInit {
         console.log("Session started", this.session);
         let publisherOptions = {
             insertMode: 'append',
-            width:90,
-            height:120,
+            width: 90,
+            height: 120,
         };
-        this.publisher = OT.initPublisher('publisher',publisherOptions);
+        this.showLoader();
+        this.publisher = OT.initPublisher('publisher', publisherOptions);
+        OT.updateViews();
         this.session.on({
             streamCreated: (event: any) => {
                 console.log('Stream created');
                 let options = {width: "100%", height: '89%', insertMode: 'append'};
-                this.session.subscribe(event.stream, 'subscriber',options);
-                setTimeout(function(){ OT.updateViews(); }, 400);
+                this.session.subscribe(event.stream, 'subscriber', options);
+                setTimeout(function () {OT.updateViews();}, 400);
 
             },
             streamDestroyed: (event: any) => {
@@ -107,6 +120,7 @@ export class OpentokPage implements OnInit {
                 console.log(`connection ended because ${event.reason}`);
             },
             sessionConnected: (event: any) => {
+                
                 let container = {"booking_id": this.bookingId, "connection_id": this.session.connection.connectionId};
                 this.bookingS.registerConnection(container).subscribe((resp: any) => {
                     console.log("Register connection result", resp);
@@ -115,8 +129,13 @@ export class OpentokPage implements OnInit {
 
                 });
                 this.session.publish(this.publisher);
-                OT.updateViews();
-                setTimeout(function(){ OT.updateViews(); }, 400);
+                this.dismissLoader();
+                let el = (document.querySelector('.OT_subscriber') as HTMLElement);
+                if (el) {
+                    el.style.setProperty('display', 'block');
+                }
+                //OT.updateViews();
+                setTimeout(function () {OT.updateViews();}, 400);
             }
         });
 
@@ -125,6 +144,23 @@ export class OpentokPage implements OnInit {
                 console.log(`There was an error connecting to the session ${error}`);
             }
         });
+    }
+    showLoader() {
+        if (document.URL.startsWith('http')) {
+            this.loadingCtrl.create({
+                spinner: 'crescent',
+                backdropDismiss: true
+            }).then(toast => toast.present());
+        } else {
+            this.spinnerDialog.show();
+        }
+    }
+    dismissLoader() {
+        if (document.URL.startsWith('http')) {
+            this.loadingCtrl.dismiss();
+        } else {
+            this.spinnerDialog.hide();
+        }
     }
 
 }
